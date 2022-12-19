9576228d2267f33bb6acbb47053d6fefc7346757
kamanso <51131377+kamanso@users.noreply.github.com>
2022-12-18 19:53:16 -0500

    Add files via upload

---
+++ vignettes/littleGames.R
@@ -0,0 +1,8 @@
+## ----setup--------------------------------------------------------------------
+library(littleGames)
+maze(size=10, cex_set=5)
+
+## -----------------------------------------------------------------------------
+knitr::include_graphics("maze(size10_cexset3).PNG")
+knitr::include_graphics("maze(size10_cexset5).PNG")
+
